05a5cee2c72ca7289e43685c3f457660ac733df8
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2023-03-02 16:40:08 +0100

    fix typo

--- app/src/resablePromise.ts
+++ app/src/resablePromise.ts
@@ -1,5 +1,5 @@
 export default class ResablePromise<T = any> extends Promise<T> {
-  public readonly setteled: boolean
+  public readonly settled: boolean
   public readonly res: (t: T) => void
   public readonly rej: (err: any) => void
   public readonly onSettled: Promise<void>
@@ -10,14 +10,14 @@ export default class ResablePromise<T = any> extends Promise<T> {
       //@ts-ignore
       rres = (r) => {
         //@ts-ignore
-        this.setteled = true
+        this.settled = true
         resOnSettled()
         res(r)
       }
       //@ts-ignore
       rrej = (r) => {
         //@ts-ignore
-        this.setteled = true
+        this.settled = true
         resOnSettled()
         rej(r)
       }
@@ -28,7 +28,7 @@ export default class ResablePromise<T = any> extends Promise<T> {
     this.res = rres
     this.rej = rrej
 
-    this.setteled = false
+    this.settled = false
 
     let resOnSettled: Function
     this.onSettled = new Promise((res) => {
